b6f71da29fc2e0eee93800fdada01c5246d03c8a
Shologu <anda.shologu@ttec.com>
2025-08-15 08:56:00 +0200

    fixing an issue with django setting on remder

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,17 +1,25 @@
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 from django.urls import path
 from chatapp.consumer import ChatConsumer
 
+# Set Django settings before importing models
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+django.setup()  # ensures apps are loaded before using models
+
+# WebSocket URL routing
+websocket_urlpatterns = [
+    path('ws/chat/<int:user_id>/', ChatConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path('ws/chat/<int:user_id>/', ChatConsumer.as_asgi()),
-        ])
+        URLRouter(
+            websocket_urlpatterns
+        )
     ),
 })
